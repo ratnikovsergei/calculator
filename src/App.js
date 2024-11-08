90d3f6773dae72e326e234a638d5f593c9ee0d3e
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2024-11-08 16:59:04 +0300

    написан функционал калькулятора

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react'
+import styles from './App.module.css'
 
 export function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+  const [operand1, setOperand1] = useState('')
+  const [operand2, setOperand2] = useState('')
+  const [operator, setOperator] = useState('')
+  const [result, setResult] = useState(null)
+  const [resultOnDisplay, setResultOnDisplay] = useState(false)
+
+  const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+  const handleNumClick = (num) => {
+    if (resultOnDisplay) {
+      setOperand1(num)
+      setOperand2('')
+      setOperator('')
+      setResultOnDisplay(false)
+    } else if (operator) {
+      setOperand2(operand2 + num)
+    } else {
+      setOperand1(operand1 + num)
+    }
+  }
+
+  const handleOperatorClick = (op) => {
+    setOperator(op)
+  }
+
+  const handleResult = () => {
+    if (operand1 && operator && operand2) {
+      let res
+      if (operator === '+') {
+        res = Number(operand1) + Number(operand2)
+      } else if (operator === '-') {
+        res = Number(operand1) - Number(operand2)
+      }
+      setResult(res)
+      setResultOnDisplay(true)
+    }
+  }
+
+  const handleReset = () => {
+    setOperand1('')
+    setOperand2('')
+    setOperator('')
+    setResult(null)
+    setResultOnDisplay(false)
+  }
+
+  return (
+    <div className={styles.calculator}>
+      <h1>Калькулятор</h1>
+      <div className={styles.display}>
+        {resultOnDisplay ? result : `${operand1} ${operator} ${operand2}`}
+      </div>
+      <div className={styles.buttons}>
+        {NUMS.map((num) => {
+          return (
+            <button key={num} onClick={() => handleNumClick(num)}>
+              {num}
+            </button>
+          )
+        })}
+        <button onClick={() => handleOperatorClick('+')}>+</button>
+        <button onClick={() => handleOperatorClick('-')}>-</button>
+        <button onClick={handleResult}>=</button>
+        <button onClick={handleReset}>C</button>
+      </div>
+    </div>
+  )
 }
